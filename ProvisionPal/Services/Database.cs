8d1ceda122eea0683ab9c40801239f60111e0ee3
Dylan Bickerstaff <dylan.bickerstaff@cedarpoint.com>
2021-12-01 16:35:37 -0500

    Add project files.

---
+++ ProvisionPal/Services/Database.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.SqlClient;
+
+namespace ProvisionPal.Services
+{
+    public class Database
+    {
+        public SqlConnection Connection = new();
+        public Database()
+        {
+            Connection.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=ProvisionPal;Integrated Security=True;MultipleActiveResultSets=True";
+        }
+    }
+}
