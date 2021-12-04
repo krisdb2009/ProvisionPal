4dfb5df19ff1ccb3c2d76fe5b8504ba08d34518c
Dylan Bickerstaff <dylan.bickerstaff@cedarpoint.com>
2021-12-04 00:46:43 -0500

    push

---
+++ ProvisionPal/Classes/ParameterUtil.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.SqlClient;
+using ProvisionPal.Services;
+using System.Text.RegularExpressions;
+
+namespace ProvisionPal.Classes
+{
+    public static class ParameterUtil
+    {
+        public static bool Validate(Database DB, Guid Parameter, string ValueToValidate)
+        {
+            DB.Connection.Open();
+            SqlCommand cmd = DB.Connection.CreateCommand();
+            cmd.Parameters.AddWithValue("@PARAMID@", Parameter);
+            cmd.CommandText = "SELECT RegularExpression FROM Parameters WHERE ParameterID = @PARAMID@";
+            string regex = (string)cmd.ExecuteScalar();
+            DB.Connection.Close();
+            return Regex.IsMatch(regex, ValueToValidate);
+        }
+    }
+}
