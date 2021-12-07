8b0d74230a639e7ba0d83f45e3ecc6587c4814bd
Dylan Bickerstaff <dylan.bickerstaff@cedarpoint.com>
2021-12-07 15:21:06 -0500

    db

--- Database/dbo/Tables/Requests.sql
+++ Database/dbo/Tables/Requests.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [dbo].[Requests] (
     [RequestID]        UNIQUEIDENTIFIER DEFAULT (newid()) NOT NULL,
+    [FormID]           UNIQUEIDENTIFIER NOT NULL,
     [RequestedBy]      TEXT             NOT NULL,
     [RequestedTime]    DATETIME         DEFAULT (sysdatetime()) NOT NULL,
     [LastModifiedBy]   TEXT             NOT NULL,
@@ -7,8 +8,11 @@
     [ProcessedBy]      TEXT             NULL,
     [ProcessedTime]    DATETIME         NULL,
     [Status]           TEXT             DEFAULT ('New') NOT NULL,
-    PRIMARY KEY CLUSTERED ([RequestID] ASC)
+    PRIMARY KEY CLUSTERED ([RequestID] ASC),
+    CONSTRAINT [FK_Requests_Forms] FOREIGN KEY ([FormID]) REFERENCES [dbo].[Forms] ([FormID])
 );
+
+
 
 
 
